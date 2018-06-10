bd693f1a1f02d2e2a7d62ef69e3ca30d38a28977
beim <beim2015@outlook.com>
2018-06-11 02:53:04 +0800

    增加ASMR项目倒计时功能

--- src/components/ManagePage.js
+++ src/components/ManagePage.js
@@ -3,6 +3,68 @@ import './ManagePage.css';
 import { httpget, move_front } from '../util.js'
 import { config } from '../config.js'
 
+class NewProjModel extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            duration: 10,
+            displayAlert: false,
+        }
+    }
+
+    inputChangeHandler(event) {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+        this.setState({
+          [name]: value
+        })
+    }
+
+    async submitHandler() {
+        let { name, duration } = this.state
+        if (!name || !duration) return
+        duration = parseInt(duration) * 60
+        let ret = await httpget(`http://${config.host}:${config.port}/api/set/proj?name=${name}&&duration=${duration}`)
+        if (ret && ret.ok === 1) {
+            this.setState({name: '', duration: 10, displayAlert: true})
+            setTimeout(() => {
+                this.setState({displayAlert: false})
+            }, 3000)
+        }
+        else {
+            console.log('err: ', ret)
+        }
+    }
+
+    render() {
+        return (
+            <div className="modal fade" id="new-proj-model" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header flex-center">
+                            <h4 className="modal-title" id="myModalLabel">新增ASMR项目</h4>
+                        </div>
+                        <div className="modal-body new-gift-model-body">
+                            <div className="flex-center">
+                            {/* <!-- 新增礼物的下拉菜单  --> */}
+                                <input value={this.state.name} onChange={this.inputChangeHandler.bind(this)} name="name" type="text" placeholder="请输入项目名"></input>
+                                <input value={this.state.duration} onChange={this.inputChangeHandler.bind(this)} name="duration" type="number" placeholder="请输入持续时间"></input>
+                            </div>
+                            <div className={`alert alert-success ${this.state.displayAlert ? '' : 'display-none'}`}>成功！可继续添加。</div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">关闭</button>
+                            <button onClick={this.submitHandler.bind(this)} type="button" className="btn btn-primary">提交更改</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
 class NewGiftModel extends Component {
 
     constructor(props) {
@@ -151,6 +213,114 @@ class DelAlertModel extends Component {
     }
 }
 
+class AlterProjRankDurationModel extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            duration: 0,
+        }
+    }
+    inputChangeHandler(event) {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+        this.setState({
+          [name]: value
+        })
+    }
+    
+    async alterDurationHandler() {
+        const duration = parseInt(this.state.duration) * 60
+        const idx = this.props.alterProjRankDuration.idx
+        let ret = await httpget(`http://${config.host}:${config.port}/api/set/proj/duration?idx=${idx}&&duration=${duration}`)
+        if (ret && ret.ok === 1) {
+            // 需要提示删除成功?
+        }
+        else {
+            console.log('err: ', ret)
+        }
+    }
+
+    render() {
+        return (
+            <div className="modal fade" id="alter-projrank-duration-model" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header flex-center">
+                            <h4 className="modal-title" id="myModalLabel">修改【{this.props.alterProjRankDuration.name}】的耗时</h4>
+                        </div>
+                        <div className="modal-body alter-reward-body">                            
+                            <input value={this.state.duration} onChange={this.inputChangeHandler.bind(this)} name="duration" type="number" placeholder="请输入耗时/分"></input>
+                        </div>
+                        <div className="modal-footer">
+                            
+                            <button type="button" className="btn btn-default" data-dismiss="modal">关闭</button>
+                            <button onClick={this.alterDurationHandler.bind(this)} type="button" className="btn btn-primary" data-dismiss="modal">提交更改</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }  
+}
+
+class AlterProjRankPosModel extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            priority: 1,
+        }
+    }
+    inputChangeHandler(event) {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+        this.setState({
+          [name]: value
+        })
+    }
+    
+    async alterPosHandler() {
+        const maxIdx = parseInt(this.props.alterProjRankPos.maxIdx)
+        const priority = parseInt(this.state.priority) - 1
+        if (priority < 0 || priority >= maxIdx) {
+            console.log(`err: priority < 0 || priority >= maxIdx[${maxIdx}]`)
+            return false
+        }
+        const idx = this.props.alterProjRankPos.idx
+        let ret = await httpget(`http://${config.host}:${config.port}/api/set/proj/priority?idx=${idx}&&priority=${priority}`)
+        if (ret && ret.ok === 1) {
+            // 需要提示删除成功?
+        }
+        else {
+            console.log('err: ', ret)
+        }
+    }
+
+    render() {
+        return (
+            <div className="modal fade" id="alter-projrank-pos-model" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header flex-center">
+                            <h4 className="modal-title" id="myModalLabel">修改【{this.props.alterProjRankPos.name}】的位置</h4>
+                        </div>
+                        <div className="modal-body alter-reward-body">                            
+                            <input  onChange={this.inputChangeHandler.bind(this)} name="priority" type="number" placeholder={`请输入位置(1-${this.props.alterProjRankPos.maxIdx})`}></input>
+                        </div>
+                        <div className="modal-footer">
+                            
+                            <button type="button" className="btn btn-default" data-dismiss="modal">关闭</button>
+                            <button onClick={this.alterPosHandler.bind(this)} type="button" className="btn btn-primary" data-dismiss="modal">提交更改</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }  
+}
+
+
 class AlterRewardModel extends Component {
 
 
@@ -288,15 +458,31 @@ class ManagePage extends Component {
                 giftName: '',
                 rewardName: '',
             },
+            projRankRunningState: false,
+            projRankOnlist: [],
+            projRankOfflist: [],
+            projRankDataTimer: null,
+            alterProjRankDuration: {
+                idx: 0,
+                duration: 0,
+                name: '',
+            },
+            alterProjRankPos: {
+                idx: 0,
+                name: '',
+                maxIdx: 0,
+            }
         }
     }
 
     componentDidMount() {
         this.setDataState()
         this.setRunningState()
+        this.setProjRankData()
         let dataTimer = setInterval(this.setDataState.bind(this), 2000)
         let runningStateTimer = setInterval(this.setRunningState.bind(this), 2000)
-        this.setState({dataTimer, runningStateTimer})
+        let projRankDataTimer = setInterval(this.setProjRankData.bind(this), 2000)
+        this.setState({dataTimer, runningStateTimer, projRankDataTimer})
     }
 
     async componentWillUnmount() {
@@ -305,6 +491,20 @@ class ManagePage extends Component {
         this.setState({dataTimer: null, runningStateTimer: null})
     }
     
+    async setProjRankData() {
+        let ret = await httpget(`http://${config.host}:${config.port}/api/get/projs`)
+        if (ret && ret.ok === 1) {
+            this.setState({
+                projRankOnlist: ret.data.onlist,
+                projRankOfflist: ret.data.offlist,
+                projRankRunningState: ret.data.onplay,
+            })
+        }
+        else {
+            console.log('err: ', ret)
+        }
+    }
+
     async setDataState() {
         let ret = await httpget(`http://${config.host}:${config.port}/api/get`)
         if (ret && ret.ok === 1) {
@@ -326,13 +526,25 @@ class ManagePage extends Component {
         }
     }
 
-    async delGiftHandler(giftName) {
+    delGiftHandler(giftName) {
         this.setState({delGiftName: giftName})
     }
 
-    async alterRewardHandler(giftName, rewardName) {
+    alterRewardHandler(giftName, rewardName) {
         this.setState({
             alterReward: {giftName, rewardName},
+        })
+    }
+
+    alterProjRankDurationHandler(idx, duration, name) {
+        this.setState({
+            alterProjRankDuration: {idx, duration, name}
+        })
+    }
+
+    adjustPosProjRankHandler(idx, name, maxIdx) {
+        this.setState({
+            alterProjRankPos: {idx, name ,maxIdx}
         })
     }
 
@@ -390,6 +602,68 @@ class ManagePage extends Component {
         return trs
     }
 
+    genProjRankTr() {
+        const parseS2M = (sec) => {
+            sec = parseInt(sec)
+            let m = Math.floor(sec / 60)
+            if (m < 10) {
+                m = '0' + m
+            }
+            let s = sec % 60
+            if (s < 10) {
+                s = '0' + s
+            }
+            return `${m}:${s}`
+        }
+        const onlist = this.state.projRankOnlist
+        const offlist = this.state.projRankOfflist
+        let trs1 = onlist.map((val, idx) => {
+            idx = parseInt(idx)
+            return (
+                <tr key={`genProjRankTr${idx}`}>
+                    <td>{idx + 1}</td>
+                    <td>
+                        {val.name}
+                    </td>
+                    <td>
+                        {parseS2M(parseInt(val.duration))}
+                        <span onClick={this.alterProjRankDurationHandler.bind(this, idx, val.duration, val.name)} className="glyphicon glyphicon-edit reward-edit-icon" data-toggle="modal" data-target="#alter-projrank-duration-model"></span>
+                    </td>
+                    <td>
+                        <button onClick={this.adjustPosProjRankHandler.bind(this, idx, val.name, onlist.length)} type="button" className="btn btn-primary btn-xs" data-toggle="modal" data-target="#alter-projrank-pos-model">位置</button>
+                    </td>
+                </tr>
+            )
+        })
+        let trs2 = offlist.map((val, idx) => {
+            idx = parseInt(idx)
+            return (
+                <tr key={`genProjRankTrOfflist${idx}`}>
+                    <td>{idx + 1}</td>
+                    <td>{val.name}</td>
+                    <td>{parseS2M(parseInt(val.duration))}</td>
+                    <td></td>
+                </tr>
+            )
+        })
+        return trs1.concat(trs2)
+    }
+
+    async projRankStartHandler() {
+        if (this.state.projRankRunningState === true) {
+            await httpget(`http://${config.host}:${config.port}/api/stop/projrank`)
+        }
+        else {
+            await httpget(`http://${config.host}:${config.port}/api/start/projrank`)
+        }
+    }
+    
+    async projRankClearHandler() {
+        await httpget(`http://${config.host}:${config.port}/api/stop/projrank`)
+        await httpget(`http://${config.host}:${config.port}/api/set/projrank/clear`)
+    }
+
+
     render() {
         return (
             <div className="out-wrapper">
@@ -419,8 +693,35 @@ class ManagePage extends Component {
                             </tbody>
                         </table>
                     </div>
-                
                 </div>
+                <div className="div-container tools">
+                    <button data-toggle="modal" data-target="#new-proj-model" type="button" className="btn btn-success">新增</button>
+                    <button onClick={this.projRankStartHandler.bind(this)} type="button" className={`btn ${this.state.projRankRunningState ? 'btn-warning' : 'btn-success'}`}>{this.state.projRankRunningState ? '停止' : '运行'}</button>
+                    <button onClick={window.open.bind(window, `http://${config.host}:5001/rank`)} type="button" className="btn btn-success">展示1</button>
+                    <button onClick={window.open.bind(window, `http://${config.host}:5001/rank1`)} type="button" className="btn btn-success">展示2</button>
+                    <button onClick={this.projRankClearHandler.bind(this)} type="button" className="btn btn-success">清空</button>
+                </div>
+                <div className="div-container">
+                    <div className="table-responsive">
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>序号</th>
+                                    <th>名称</th>
+                                    <th>剩余时间</th>
+                                    <th>操作</th>
+                                </tr>
+                                
+                            </thead>
+                            <tbody>
+                                {this.genProjRankTr()}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <NewProjModel></NewProjModel>
+                <AlterProjRankDurationModel alterProjRankDuration={this.state.alterProjRankDuration}></AlterProjRankDurationModel>
+                <AlterProjRankPosModel alterProjRankPos={this.state.alterProjRankPos}></AlterProjRankPosModel>
                 <NewGiftModel giftExisted={this.state.data.map(val => val.gift_name)}></NewGiftModel>
                 <DelAlertModel delGiftName={this.state.delGiftName}></DelAlertModel>
                 <AlterRewardModel alterReward={this.state.alterReward}></AlterRewardModel>
